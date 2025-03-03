5837c00039cd0640efebc6ae858b83d2f882a712
Vladimir Mikhalev <voavili32@gmail.com>
2025-03-03 19:28:47 +0300

    fix: changed tab to 4 spaces

--- src/main.cpp
+++ src/main.cpp
@@ -21,56 +21,55 @@ constexpr int run_speed = 5;
 
 inline void control_player(bn::sprite_ptr& player, bn::sprite_animate_action<2>& action)
 {
-	//
-	if(bn::keypad::start_pressed())
-	{
-		player.set_visible(!player.visible());
-	}
-	
-	// movement test
-	if(bn::keypad::left_held())
-	{
-		player.set_x(player.x() - speed);
-	}
-	else if(bn::keypad::right_held())
+    //
+    if(bn::keypad::start_pressed())
     {
-		player.set_x(player.x() + speed);
-	}
-	
-	// animation test
-	if(bn::keypad::left_pressed())
-	{
-		action = bn::create_sprite_animate_action_forever(
-			player, run_speed, bn::sprite_items::dino.tiles_item(), 1, 2);
-	}
-	else if(bn::keypad::right_pressed())
-	{
-		action = bn::create_sprite_animate_action_forever(
-			player, run_speed, bn::sprite_items::dino.tiles_item(), 1, 2);
-	}
-	else if(!bn::keypad::left_held() && !bn::keypad::right_held()) // maybe butano version of &&?
-	{
-		action = bn::create_sprite_animate_action_once(
-			player, run_speed, bn::sprite_items::dino.tiles_item(), 0, 0);
-	}
-	
-	action.update();
+        player.set_visible(!player.visible());
+    }
+    
+    // movement test
+    if(bn::keypad::left_held())
+    {
+        player.set_x(player.x() - speed);
+    }
+    else if(bn::keypad::right_held())
+    {
+        player.set_x(player.x() + speed);
+    }
+    
+    // animation test
+    if(bn::keypad::left_pressed())
+    {
+        action = bn::create_sprite_animate_action_forever(
+            player, run_speed, bn::sprite_items::dino.tiles_item(), 1, 2);
+    } 
+    else if(bn::keypad::right_pressed())
+    {
+        action = bn::create_sprite_animate_action_forever(
+            player, run_speed, bn::sprite_items::dino.tiles_item(), 1, 2);
+    }
+    else if(!bn::keypad::left_held() && !bn::keypad::right_held())
+    {
+        action = bn::create_sprite_animate_action_once(
+            player, run_speed, bn::sprite_items::dino.tiles_item(), 0, 0);
+    }
+    
+    action.update();
 }
 
 int main()
 {
     bn::core::init();
-	
-	bn::sprite_ptr player = bn::sprite_items::dino.create_sprite(0, 32);
-	bn::regular_bg_ptr bg = bn::regular_bg_items::bg_temp.create_bg(0, 0);
-	
-	bn::sprite_animate_action<2> action = bn::create_sprite_animate_action_once(
-		player, run_speed, bn::sprite_items::dino.tiles_item(), 0, 0);
+    
+    bn::sprite_ptr player = bn::sprite_items::dino.create_sprite(0, 32);
+    bn::regular_bg_ptr bg = bn::regular_bg_items::bg_temp.create_bg(0, 0);
+    
+    bn::sprite_animate_action<2> action = bn::create_sprite_animate_action_once(
+        player, run_speed, bn::sprite_items::dino.tiles_item(), 0, 0);
 
     while(true)
     {
-		control_player(player, action);
-		
+        control_player(player, action);
         bn::core::update();
     }
 }
