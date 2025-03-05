331955b8fc4e894b1ceb68306c454db390ec93f7
Vladimir Mikhalev <voavili32@gmail.com>
2025-03-05 13:47:28 +0300

    feat: create Player class

--- src/main.cpp
+++ src/main.cpp
@@ -15,63 +15,93 @@
 #include "bn_regular_bg_items_bg_temp.h"
 
 
-constexpr bn::fixed_t<8> speed = 2;
-constexpr int run_speed = 5;
+constexpr bn::fixed speed = 2;
+constexpr int anim_speed = 5;
 
 
-inline void control_player(bn::sprite_ptr& player, bn::sprite_animate_action<2>& action)
+class Player
 {
-    //
-    if(bn::keypad::start_pressed())
+public:
+    Player()
+        : spr(bn::sprite_items::dino.create_sprite(x, y))
+        , act(bn::create_sprite_animate_action_forever(
+            spr, anim_speed, bn::sprite_items::dino.tiles_item(),
+            0, 0))
+    {}
+    
+    void update()
     {
-        player.set_visible(!player.visible());
+        change_state();
+        move();
+        animate();
     }
     
-    // movement test
-    if(bn::keypad::left_held())
+private:
+    bn::fixed x = 0;
+    bn::fixed y = 32;
+    bool face_left = true;
+    bn::sprite_ptr spr;
+    bn::sprite_animate_action<2> act;
+    enum player_states { idle, start_run, run, jump };
+    player_states state = idle;
+    
+    void change_state()
     {
-        player.set_x(player.x() - speed);
-    }
-    else if(bn::keypad::right_held())
-    {
-        player.set_x(player.x() + speed);
+        if(bn::keypad::left_pressed())
+        {
+            state = start_run;
+            face_left = true;
+        }
+        else if(bn::keypad::right_pressed())
+        {
+            state = start_run;
+            face_left = false;
+        }
+        else if(bn::keypad::left_held() || bn::keypad::right_held())
+        {
+            state = run;
+        }
+        else state = idle;
     }
     
-    // animation test
-    if(bn::keypad::left_pressed())
+    void move()
     {
-        player.set_horizontal_flip(true);
-        action = bn::create_sprite_animate_action_forever(
-            player, run_speed, bn::sprite_items::dino.tiles_item(), 1, 2);
-    } 
-    else if(bn::keypad::right_pressed())
-    {
-        player.set_horizontal_flip(false);
-        action = bn::create_sprite_animate_action_forever(
-            player, run_speed, bn::sprite_items::dino.tiles_item(), 1, 2);
-    }
-    else if(!bn::keypad::left_held() && !bn::keypad::right_held())
-    {
-        action = bn::create_sprite_animate_action_once(
-            player, run_speed, bn::sprite_items::dino.tiles_item(), 0, 0);
+        if(state == run || state == start_run)
+        {
+            spr.set_x(spr.x() + speed * (-1 + 2 * !face_left));
+        }
     }
     
-    action.update();
-}
+    void animate()
+    {
+        spr.set_horizontal_flip(face_left);
+        
+        if(state == start_run)
+        {
+            act = bn::create_sprite_animate_action_forever(
+                spr, anim_speed, bn::sprite_items::dino.tiles_item(), 1, 2);
+        }
+        else if(state == idle)
+        {
+            act = bn::create_sprite_animate_action_forever(
+                spr, anim_speed, bn::sprite_items::dino.tiles_item(), 0, 0);
+        }
+        
+        act.update();
+    }
+};
+
 
 int main()
 {
     bn::core::init();
     
-    bn::sprite_ptr player = bn::sprite_items::dino.create_sprite(0, 32);
+    Player dino;
     bn::regular_bg_ptr bg = bn::regular_bg_items::bg_temp.create_bg(0, 0);
-    
-    bn::sprite_animate_action<2> action = bn::create_sprite_animate_action_once(
-        player, run_speed, bn::sprite_items::dino.tiles_item(), 0, 0);
 
     while(true)
     {
-        control_player(player, action);
+        dino.update();
         bn::core::update();
     }
 }
